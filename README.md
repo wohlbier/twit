6287b0e3fab38e8d6950e2acd2723d4560f7332f
John Wohlbier <jgwohlbier@sei.cmu.edu>
2020-09-05 09:24:24 -0700

    finish china

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # twit
+
+```
+conda create -n twit_env -c conda-forge gsutil python=3 unzip
+conda activate twit_env
+```
--- getdata.sh
+++ getdata.sh
@@ -1,29 +1,66 @@
 #!/bin/bash
 
-BUCKET="twitter-election-integrity/hashed/2020_05/china_052020/china_052020_tweet_media_hashed/"
-FILES="china_052020_hashed_001.zip
-china_052020_hashed_002.zip
-china_052020_hashed_003.zip
-china_052020_hashed_004.zip
-china_052020_hashed_005.zip
-china_052020_hashed_006.zip
-china_052020_hashed_007.zip
-china_052020_hashed_008.zip
-china_052020_hashed_009.zip
-china_052020_hashed_010.zip
-china_052020_hashed_011.zip
-china_052020_hashed_012.zip
-china_052020_hashed_013.zip
-china_052020_hashed_014.zip
-china_052020_hashed_015.zip
-china_052020_hashed_016.zip
-china_052020_hashed_017.zip"
+if [ "$1" = "china" ]; then
+    echo "getting china data"
+    BUCKET="twitter-election-integrity/hashed/2020_05/china_052020"
+    BASE="china_052020"
+    NZ=17
+elif [ "$1" = "russia" ]; then
+    echo "getting russia data"
+elif [ "$1" = "turkey" ]; then
+    echo "getting turkey data"
+else
+    echo "need to specify china, russia, or turkey"
+    exit 1
+fi
 
-gsutil cp $BUCKET/china_052020_hashed_README.txt .
+f=Twitter_Elections_Integrity_Datasets_hashed_README.txt
+if [ ! -f ./${f} ]; then
+    echo "Getting ${f}"
+    gsutil cp gs://twitter-election-integrity/hashed/2020_05/${f} .
+fi
 
-for f in $FILES
-do
-    echo $f
-    gsutil cp gs://$BUCKET$f .
-    unzip $f
+# check if exists already, ask for removal
+if [ ! -d ${BASE} ]; then
+    mkdir -p ${BASE}
+fi
+cd ${BASE}
+
+# hashed users
+f=${BASE}_users_csv_hashed.zip
+if [ ! -f ./${f} ]; then
+    gsutil cp gs://${BUCKET}/${f} .
+    unzip ${f}
+fi
+
+# tweets and metadata
+f=${BASE}_tweets_csv_hashed.zip
+if [ ! -f ./${f} ]; then
+    gsutil cp gs://${BUCKET}/${f} .
+    unzip ${f}
+fi
+
+# media
+# README
+p=${BASE}_tweet_media_hashed
+f=${BASE}_hashed_README.txt
+if [ ! -f ./${f} ]; then
+    gsutil cp gs://${BUCKET}/${p}/${f} .
+fi
+
+# files
+for i in $(seq 1 $NZ); do
+    if [ "$i" -lt "10" ]; then
+        n=00$i
+    elif [ "$i" -lt "100" ]; then
+        n=0$i
+    else
+        n=0$i
+    fi
+
+    f=${BASE}_hashed_${n}.zip
+    if [ ! -f ./${f} ]; then
+        gsutil cp gs://${BUCKET}/${p}/${f} .
+        unzip ${f}
+    fi
 done
